ad1fabfc9f39cde6f38d997d20cfdc4bff9a61b5
Arthur Gonçalo <arte.arthur@hotmail.com>
2020-10-23 20:04:33 -0300

    Adicionando atualizações

--- FUNCAO/cidadaoPrimeiraClasse.js
+++ FUNCAO/cidadaoPrimeiraClasse.js
@@ -3,17 +3,17 @@
 
 // criar de forma literal
 
-function fun1() { } 
+function fun1() { }
 
 // Armazenar em uma variável
- const fun2 = function () { }
+const fun2 = function () { }
 
 // Armazenar em um array 
-const array = [function(a, b) { return a + b + c}, fun1, fun2]
+const array = [function (a, b) { return a + b + c }, fun1, fun2]
 console.log(array[0](2, 3))
 
 // Armazenar em um atributo de objeto 
-const obj = { }
+const obj = {}
 obj.falar = function () { return 'Opa' }
 console.log(obj.falar())
 
@@ -26,11 +26,14 @@ run(function () { console.log('Executando...') })
 
 // Uma função pode retornar/conter uma função 
 function soma(a, b) {
-    return function (c) { 
+    return function (c) {
         console.log(a + b + c)
     }
 }
 
-soma (2, 3)(4)
+soma(2, 3)(4)
 const cincoMais = soma(2, 3)
 cincoMais(4)
+
+
+// Teste
