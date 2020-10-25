284ae8e35d21bcf7de4e3f3bb6019044ec37fe22
Arthur Gonçalo <arte.arthur@hotmail.com>
2020-10-25 13:52:46 -0300

    Adicionando atualizações

---
+++ FUNCAO/arrowFunction3.js
@@ -0,0 +1,18 @@
+let comparaComThis = function (param) {
+    console.log(this == param)
+}
+
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComArrow = param => console.log(thiss == param)
+comparaComArrow(global)
+comparaComArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
