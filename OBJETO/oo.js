76d23dedeb6b188a4333d78334745a7da510ab25
Arthur Gonçalo <arte.arthur@hotmail.com>
2020-11-04 21:21:06 -0300

    Adicionando atualizações

---
+++ OBJETO/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTAVEL !!!
+
+// Procedural 
+processamento(valor1, valor2, valor3)
+
+// 00 
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ....
+    }
+}
+
+objeto.processamento()
+
+// Principios importantes:
+// 1. Abstracao
+// 2. Encapsulamento 
+// 3. Heranca (prototype)
+// 4. Polimorfismo
